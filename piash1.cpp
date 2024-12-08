2ff947ff260837e30f97048c9c322e674e74e733
piash25252 <piashislam775@gmail.com>
2024-12-08 12:11:46 +0600

    piash setup

---
+++ piash1.cpp
@@ -0,0 +1,8 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    cout<<"Phello wo";
+
+    return 0;
+}
---
+++ string_constructor.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    // string s="hello orld";
+    //1
+    // string s("Hello piash");
+    //2
+    // string s("piash islam",4);
+    //3
+    // string a="Piash Islam";
+    // string s(a,7);
+    //4
+    string s(26,'P');
+
+    cout<<s<<endl;
+
+    return 0;
+}
